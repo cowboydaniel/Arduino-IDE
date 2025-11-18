2a03121ad0cc347e144e527326c80965afb12223
cowboydaniel <107297413+cowboydaniel@users.noreply.github.com>
2025-11-18 18:15:07 +1100

    Update pyjnius recipe to 1.7.0

---
+++ android/p4a-recipes/pyjnius/__init__.py
@@ -0,0 +1,11 @@
+from pythonforandroid.recipes.pyjnius import PyjniusRecipe as BasePyjniusRecipe
+
+
+class PyjniusRecipe(BasePyjniusRecipe):
+    # Use the latest pyjnius release to avoid Cython 3 incompatibilities
+    version = "1.7.0"
+    url = "https://github.com/kivy/pyjnius/archive/{version}.zip"
+    patches = BasePyjniusRecipe.patches
+
+
+recipe = PyjniusRecipe()
